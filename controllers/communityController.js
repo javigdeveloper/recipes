00dbb78863d2f8813a6bb5b217c4ff3297a4a775
Javier García <javigdeveloper@gmail.com>
2020-08-15 22:22:53 -0500

    mvc approach

---
+++ controllers/communityController.js
@@ -0,0 +1,62 @@
+const Recipe = require("../models/recipe");
+
+const community_index = (req, res) => {
+  Recipe.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("community", { communityRecipes: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const community_details = (req, res) => {
+  const id = req.params.id;
+  Recipe.findById(id)
+    .then((result) => {
+      res.render("preparation", { recipeFromDB: result });
+    })
+    .catch((err) => {
+      res.status(404).render("404");
+    });
+};
+
+const community_create_get = (req, res) => {
+  res.render("create");
+};
+
+const community_create_post = (req, res) => {
+  // creating an instance of a recipe with req.body,
+  // which is the object comming from the form
+  const recipe = new Recipe(req.body);
+  recipe
+    .save()
+    .then((result) => {
+      res.redirect("/community");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// This handler cannot send a redirect as a response, it can send json
+// or text data back to the broser
+const community_delete = (req, res) => {
+  const id = req.params.id;
+  Recipe.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/community" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = {
+  community_index,
+  community_details,
+  community_create_get,
+  community_create_post,
+  community_delete,
+};
--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -1,62 +1,12 @@
 const express = require("express");
-const Recipe = require("../models/recipe");
+const communityController = require("../controllers/communityController");
 const router = express.Router();
 
 // recipe routes
-router.get("/", (req, res) => {
-  Recipe.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("community", { communityRecipes: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.post("/", (req, res) => {
-  // creating an instance of a recipe with req.body,
-  // which is the object comming from the form
-  const recipe = new Recipe(req.body);
-
-  recipe
-    .save()
-    .then((result) => {
-      res.redirect("/community");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-router.get("/create", (req, res) => {
-  res.render("create");
-});
-
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  Recipe.findById(id)
-    .then((result) => {
-      console.log(result);
-      res.render("preparation", { recipeFromDB: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-// This handler cannot send a redirect as a response, it can send json
-// or text data back to the broser
-router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-
-  Recipe.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/community" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/", communityController.community_index);
+router.post("/", communityController.community_create_post);
+router.get("/create", communityController.community_create_get);
+router.get("/:id", communityController.community_details);
+router.delete("/:id", communityController.community_delete);
 
 module.exports = router;
